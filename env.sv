2040772bdf96fc698204d6e3ae5c6337931bc7be
Sukhithaaithal <108125369+Sukhithaaithal@users.noreply.github.com>
2024-03-12 22:48:37 +0530

    Update env.sv

--- env.sv
+++ env.sv
@@ -2,7 +2,11 @@ class env extends uvm_env;
 
   `uvm_component_utils(env)
   
+  function new(string name = "env", uvm_component parent = null);
+    super.new(name,parent);
+  endfunction
 
+  
 
 
 
